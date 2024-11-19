dd3feba3b08b8bf16822b05a1d5912bec6d923ba
Daz <shoeshinesamurai@gmail.com>
2024-11-19 11:52:42 -0800

    web dev2 callback exercise done

--- file_reader.js
+++ file_reader.js
@@ -1,5 +1,14 @@
 const fs = require("fs");
 
 fs.readFile("./firstname.txt", "utf-8", (err, data1) => {
-  // your code here
+  fs.readFile("./lastname.txt", "utf-8", (err, data2) => {
+    fs.readFile("./age.txt", "utf-8", (err, data3) => {
+      fs.readFile("./hobbies.txt", "utf-8", (err, data4) => {
+        const modifiedData4 = data4.replace(/[\[\]"]/g, '');
+        const hobbies = modifiedData4.split(",");
+        console.log(hobbies[0]);
+        console.log(`${data1} ${data2} is ${data3} years old and his hoobies are ${hobbies[0]} and ${hobbies[1]}`);
+      });
+    });
+  });
 })
